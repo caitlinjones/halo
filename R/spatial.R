b871ca97dbe41705148289012cb8f97743a72c38
Caitlin Jones <caitjbyrne@gmail.com>
2018-02-12 19:13:47 -0500

    adding first iteration of spatial plots

---
+++ R/spatial.R
@@ -0,0 +1,381 @@
+#' Recursively process an XML node for one FOV
+#'
+#' Visit each node in an object of class XMLNode and gather information
+#' to print one line for each vertex which represents the location of a cell
+#' on the slide [NOTE: DOUBLE CHECK THIS FOR ACCURACY] 
+#' 
+#' Output is a tab-delimited file containing: 
+#'     1)  FOV name
+#'     2)  Annotation number
+#'     3)  Line color
+#'     4)  Name
+#'     5)  Visible
+#'     6)  Region number
+#'     7)  Region type
+#'     8)  HasEndcaps
+#'     9)  NegativeROA
+#'     10) X coordinate
+#'     11) Y coordinate ***(negative of what is output by Halo)***
+#'     12) Region according to color (Tumor, Exclusion, Epidermis)
+#' 
+#' @param node           object of class XMLNode containing Halo annotations for one FOV
+#' @param fovName        unique identifier of FOV
+#' @param outFile        output file
+#' @param regionNum      current region number
+#' @param annotationNum  current annotation number
+#' @param LineColor      code for line color of the annotation ["655280"|"65535"|"255"]
+#' @param Name           name of the annotation
+#' @param Visible        logical indicating whether annotation is visible [????]
+#' @param RegionType     region type *Note: any region with type other than Polygon is skipped
+#' @param HasEndcaps     [???]
+#' @param NegativeROA    [???]
+#' @param v              verbose; print extra status messages; default=FALSE
+#' @param lf             log file; default=NULL
+#' @param debug          logical indicating whether to print debug statements
+#' @return nothing
+parseAnnotationXML <- function(node, fovName=NULL, outFile=NULL, 
+                        LineColor="", Name="", Visible="", RegionType="", HasEndcaps=0,
+                        NegativeROA=0, v=FALSE, lf=NULL, debug=FALSE){
+  ## use global variables in order to keep track of annotation and region nums
+  ## while traversing xml doc; remove them later
+  annotationNum <<- 0
+  regionNum <<- 0
+
+  goTime <- function(node, fovName=NULL, outFile=NULL, 
+                        LineColor="", Name="", Visible="", RegionType="", HasEndcaps=0,
+                        NegativeROA=0, v=FALSE, lf=NULL, debug=FALSE){
+
+    if(is.null(node) | length(node) == 0){
+        return()
+    }
+    colorToRegionMap <- list("65280"="Tum", "65535"="Exc", "255"="Epi")
+
+    nodeName <- xmlName(node)
+    nodeAttrs <- xmlAttrs(node)
+    
+    if(nodeName == "Annotation"){
+        annotationNum <<- annotationNum + 1
+        #regionNum <<- 0
+        if(debug){ logMsg(paste0("AnnotNum: ",annotationNum),v,lf,"DEBUG") }
+        Name <- xmlGetAttr(node, "Name")
+        Visible <- xmlGetAttr(node, "Visible")
+        LineColor <- xmlGetAttr(node, "LineColor")
+    } else if(nodeName == "Region"){
+        regionNum <<- regionNum + 1
+        if(debug){ logMsg(paste0("RegionNum: ",regionNum),v,lf,"DEBUG") }
+        HasEndcaps <- xmlGetAttr(node, "HasEndcaps")
+        NegativeROA <- xmlGetAttr(node, "NegativeROA")
+        RegionType <- xmlGetAttr(node, "Type")
+    } else if(nodeName == "V"){
+        X = xmlGetAttr(node, "X")
+        Y = -as.numeric(xmlGetAttr(node, "Y"))
+        if(RegionType == "Polygon"){
+            line <- paste(fovName, annotationNum, LineColor, Name, Visible, 
+                          regionNum, RegionType, HasEndcaps, NegativeROA, X, Y, 
+                          colorToRegionMap[[LineColor]], sep="\t")
+            write(line, file=outFile, append=TRUE)
+        }
+        return()
+    }
+
+    if(xmlSize(node) > 0){
+       for(i in 1:xmlSize(node)){
+           goTime(node[[i]], fovName=fovName, LineColor=LineColor, 
+                       Name=Name, Visible=Visible, RegionType=RegionType, 
+                       HasEndcaps=HasEndcaps, NegativeROA=NegativeROA,
+                       outFile=outFile, v=v, lf=lf, debug=debug)
+       }
+    }
+  }
+
+  res <- goTime(node, fovName=fovName, outFile=outFile, 
+           LineColor=LineColor, Name=Name, Visible=Visible, RegionType=RegionType, 
+           HasEndcaps=HasEndcaps, NegativeROA=NegativeROA, v=v, lf=lf, debug=debug)  
+
+  rm(annotationNum)
+  rm(regionNum)
+}
+
+#' Parse boundary annotations of a single FOV in a single XML file
+#' 
+#' Parse XML file containing annotations for one FOV. Write tab-delimited file
+#' of select information (see ?parseAnnotationXML for details)
+#' 
+#' @param annoteFile    XML file containing boundary annotations for a single FOV
+#' @param v             verbose - when set to TRUE, messages will be printed to screen 
+#'                      as well as to file; DEFAULT = TRUE
+#' @param logFile       log file
+#' @param debug         print debug messages; default=FALSE
+#' @export
+readHaloAnnotations <- function(annoteFile, v=TRUE, logFile=NULL, debug=FALSE){
+    tfile <- tempfile(tmpdir=".")
+    header = paste("File","AnnotationNum","LineColor","Name","Visible","RegionNum","Type",
+                   "HasEndcaps","NegativeROA","X","Y","RegionCode",sep="\t")
+    write(header, file=tfile, append=FALSE)
+
+    rootNode <- xmlRoot(xmlParse(annoteFile))
+    fovName <- gsub("\\.annotations","",basename(annoteFile))
+    parseAnnotationXML(rootNode, fovName=fovName, outFile=tfile, 
+                           v=v, lf=logFile, debug=debug)
+
+    aa <- read_tsv(tfile, col_types="ciicciciiiic")
+    fixFile <- file.path(dirname(annoteFile), "haloBoundaryRegionReassignments.csv")
+    if(debug){ logMsg(paste0("Reassign file=",fixFile,"\n"),v,logFile) }
+    if(file.exists(fixFile)){
+        fix <- read_csv(fixFile, col_types="ccic")
+        ff <- fix %>% filter(Spot==aa$File[1])
+        if(nrow(ff)>0){
+            for(kk in seq(nrow(ff))){
+                aa$RegionCode[ aa$RegionNum==ff$RegionNum[kk] ] <- ff$RegionCode[kk]
+            }
+        }
+    }
+    unlink(tfile)
+    split(aa,aa$RegionNum)
+}
+
+
+#' Get min and max X and Y coordinates of a FOV
+#' 
+#' @param  spObj  spot object(?); data frame containing X and Y coordinates
+#'                of all cells in a FOV
+#' @return list where X0 = minimum X, X1 = maximum X, and same for Y 
+getBoundingBoxL<-function(spObj) {
+    list(X0=min(spObj$X),Y0=min(spObj$Y),X1=max(spObj$X),Y1=max(spObj$Y))
+}
+
+#' Get X and Y coordinates of box enclosing two existing boxes
+#'
+joinBoundingBoxes<-function(b1,b2){
+    list(
+        X0=min(b1$X0,b2$X0),
+        Y0=min(b1$Y0,b2$Y0),
+        X1=max(b1$X1,b2$X1),
+        Y1=max(b1$Y1,b2$Y1)
+        )
+}
+
+drawBoundaries<-function(bnd,textLabels=T) {
+    for(jj in seq(bnd)) {
+        drawBoundary(bnd[[jj]],textLabels=textLabels)
+    }
+}
+
+drawBoundary<-function(b,bCols=bndColors,col=NULL,lwd=2,textLabels=T,...) {
+    if(is.null(col)) {
+        bCol=bCols[[b$RegionCode[1]]]
+    } else {
+        bCol=col
+    }
+    lines(b$X,b$Y,lwd=lwd,col=bCol,...)
+    if(textLabels) {
+        text(mean(b$X),mean(b$Y),b$RegionNum[1],cex=1.4,col=bCol)
+    }
+}
+
+drawBoundingBox<-function(b,...){
+    rect(b$X0,b$Y0,b$X1,b$Y1,...)
+}
+
+padBoundingBox<-function(bb,pad){
+    bn=list()
+    bn$X0=bb$X0-pad
+    bn$Y0=bb$Y0-pad
+    bn$X1=bb$X1+pad
+    bn$Y1=bb$Y1+pad
+    bn
+}
+
+#' Distance between points (L2-norm)
+distance<-function(p1,p2){sqrt(sum((p1-p2)^2))}
+
+#' Compute theta
+theta<-function(p1,p2){vv=p1-p2;atan(vv[2]/vv[1])*(180/pi)}
+
+# Value:
+
+#      integer array; values are: 0: point is strictly exterior to pol;
+#      1: point is strictly interior to pol; 2: point lies on the
+#      relative interior of an edge of pol; 3: point is a vertex of pol.
+
+
+pointsInPolygon<-function(pts,poly){
+    point.in.polygon(pts$X,pts$Y,poly$X,poly$Y)==1
+}
+
+pointsInsidePolygonList<-function(pts,bList) {
+
+    inside=bList %>%
+        map(function(bn){point.in.polygon(pts$X,pts$Y,bn$X,bn$Y)>0}) %>%
+        as.data.frame %>%
+        apply(.,1,any)
+
+    inside
+
+}
+
+intersectWithBoundingBox<-function(x,bb) {
+     as.tibble(x) %>%
+        mutate(X=ifelse(X>bb$X1,bb$X1,ifelse(X<bb$X0,bb$X0,X))) %>%
+        mutate(Y=ifelse(Y>bb$Y1,bb$Y1,ifelse(Y<bb$Y0,bb$Y0,Y))) %>%
+        distinct
+}
+
+expandClippedPathToOuterBox<-function(x,bin,bout){
+    x %>%
+        mutate(X=ifelse(X>bin$X1,bout$X1,ifelse(X<bin$X0,bout$X0,X))) %>%
+        mutate(Y=ifelse(Y>bin$Y1,bout$Y1,ifelse(Y<bin$Y0,bout$Y0,Y))) %>%
+        distinct
+}
+
+boundingBoxToRect<-function(bb){
+    rr=rbind(c(bb$X0,bb$Y0),c(bb$X1,bb$Y0),c(bb$X1,bb$Y1),c(bb$X0,bb$Y1))
+    colnames(rr)=c("X","Y")
+    as.data.frame(rr)
+}
+
+boundingBoxToPath<-function(bb,Z=10){
+    poly=rbind(
+        cbind(bb$X0:bb$X1,bb$Y0),
+        cbind(bb$X1,bb$Y0:bb$Y1),
+        cbind(bb$X1:bb$X0,bb$Y1),
+        cbind(bb$X0,bb$Y1:bb$Y0))
+    colnames(poly)=c("X","Y")
+    as.data.frame(poly)
+}
+
+areaBB<-function(bb){
+    (bb$X1-bb$X0)*(bb$Y1-bb$Y0)
+}
+
+boundaryToSpatialPolygon<-function(bnd,tag) {
+    SpatialPolygons(list(
+         Polygons(list(Polygon(bnd[,c("X","Y")])),tag)))
+}
+
+clipSPBoundaryToBB<-function(boundarySP,bb) {
+
+    box=boundingBoxToRect(bb)
+    spClip=SpatialPolygons(list(
+        Polygons(list(Polygon(box)),"Clip")))
+
+    res=try({clipBnd=gIntersection(boundarySP,spClip)})
+        if(class(res)=="try-error") {
+            cat("gIntersect::ERROR\n")
+            bnd.s=gSimplify(boundarySP,tol=0.0000001)
+            bnd.s=gBuffer(bnd.s,byid=T,width=0)
+            clipBnd=gIntersection(bnd.s,spClip)
+        }
+
+    if(!is.null(clipBnd)) {
+        if(class(clipBnd)=="SpatialCollections"){
+            clipBnd=clipBnd@polyobj
+        }
+        return(clipBnd)
+    } else {
+        return(boundarySP)
+    }
+
+}
+
+clipSPBoundaryToBBWithDraw<-function(boundarySP,bb,bColor=4) {
+
+    clipBnd=clipBoundaryToBB(boundarySP,bb)
+
+    if(!is.null(clipBnd)) {
+        lines(clipBnd,col=bColor,lwd=3)
+    }
+
+    return(clipBnd)
+
+}
+
+#
+# Note trim works on data frames as the result may be open
+#
+trimBoundaryToBB<-function(boundaryDF,bb) {
+
+     xx=boundaryDF %>%
+     filter(X>=bb$X0 & X<=bb$X1) %>%
+     filter(Y>=bb$Y0 & Y<=bb$Y1)
+     xx
+
+}
+
+trimDFToBB<-function(df,bb) {
+
+     xx=df %>%
+     filter(X>=bb$X0 & X<=bb$X1) %>%
+     filter(Y>=bb$Y0 & Y<=bb$Y1)
+     xx
+
+}
+
+subDivideLongSegments<-function(boundary,maxSegLength) {
+
+    bline=boundary %>% select(X,Y) %>% mutate(SegNo=seq(nrow(.)))
+    newPts=list()
+    for(is in seq(nrow(bline)-1)) {
+
+        pt1=bline[is,]
+        pt2=bline[is+1,]
+        subDivide=ceiling(distance(pt1,pt2)/maxSegLength)
+        if(subDivide>1) {
+            ##cat(is,"Dist=",distance(pt1,pt2),"subDivide =",subDivide,"\n")
+            dx=pt2$X-pt1$X
+            dy=pt2$Y-pt1$Y
+
+            newPts[[is]]=data.frame(
+                    X=pt1$X+dx*seq(subDivide-1)/subDivide,
+                    Y=pt1$Y+dy*seq(subDivide-1)/subDivide,
+                    SegNo=is+seq(subDivide-1)/subDivide
+                    )
+        }
+    }
+
+    bind_rows(bline,bind_rows(newPts)) %>% arrange(SegNo)
+
+}
+
+findDistaceFromPointsToInterfacePoint<-function(pts,interface) {
+
+    interfaceTree=createTree(interface)
+
+    knn=knnLookup(interfaceTree,newdat=pts,k=1)
+
+    as.vector(apply(cbind(pts,interface[knn,]),1,function(pair){distance(pair[c(1,2)],pair[c(3,4)])}))
+
+}
+
+getContours<-function(df,levels){
+
+    contours=getContourLines(df,levels=levels)
+    split(contours,contours$Group) %>% map(rename,X=x,Y=y,Z=z)
+
+}
+
+#
+# These are deprecated
+#
+clipToBounding<-function(x,bb){
+    stop("spatialV3:168:SHOULD USE trimBoundaryToBB")
+    xx=x %>%
+    filter(X>=bb[1] & X<=bb[3]) %>%
+    filter(Y>=bb[2] & Y<=bb[4]) %>%
+    select(X,Y)
+    #
+    # Add an pair of NA's to last row to
+    # break links between interface regions
+    xx=rbind(xx,c(NA,NA))
+    xx
+}
+
+clipToBoundingNoSelect<-function(x,bb){
+    stop("spatialV3:181:SHOULD USE trimBoundaryToBB")
+    xx=x %>%
+    filter(X>=bb[1] & X<=bb[3]) %>%
+    filter(Y>=bb[2] & Y<=bb[4])
+    xx
+}
+
+
---
+++ scripts/spatial_plots.R
@@ -0,0 +1,62 @@
+#!/home/byrne/R/R-3.4.3/bin/R
+
+options( java.parameters = c("-Xss2560k", "-Xmx8g") )
+options('python_cmd'='/opt/common/CentOS_6-dev/python/python-3.5.1/bin/python')
+
+write("Loading libraries...",stdout())
+suppressPackageStartupMessages(library("argparse"))
+suppressPackageStartupMessages(library("halo"))
+
+###
+# Parse user input
+###
+parser <- ArgumentParser()
+## args are preferrably all in manifest
+parser$add_argument("-m", "--manifest", type="character", default=NULL, help="file containing all project parameters; run ?projectParams for details")
+## args required if manifest not given
+parser$add_argument("--cell_types_file", type="character", default=NULL, help="file containing list of cell type markers to be included on plots; each line is one marker combination and may contain a single marker or a comma-separated list of markers (e.g., 'CD3' or 'CD3,CD8-,SOX10-'")
+parser$add_argument("-d", "--data_dir", type="character", help="directory of *.rda files, each containing halo data for one sample")
+parser$add_argument("-a", "--annotations_dir", type="character", help="directory of Halo *.annotations files in XML format")
+## optional args
+parser$add_argument("-v", "--verbose", action="store_true", default=FALSE, help="print extra output")
+parser$add_argument("-l", "--log", type="character", default=gsub(" ","_",date()), help="log file")
+parser$add_argument("--debug", action="store_true", default=FALSE, help="print extra output for debugging")
+
+args <- parser$parse_args()
+####################################################
+
+pp <- NULL
+
+## get all params from manifest if it exists, otherwise get them from command line
+print(args$manifest)
+if(!is.null(args$manifest)){
+    pp <- projectParams(args$manifest)
+    logMsg(paste0("Pulled params from manifest ",args$manifest))
+} else {
+    pp <- args
+}
+
+if(!is.null(pp)){
+    logParams(pp)
+}
+
+## validate input
+if(is.null(pp$cell_types_file)){
+    stop("Please specify 'cell_types_file'.") 
+}
+if(is.null(pp$data_dir)){
+    stop("Please specify 'data_dir'.")
+}
+if(is.null(pp$annotations_dir)){
+    stop("Please specify 'annotations_dir'.")
+}
+
+### take data dir as argument
+### find data files and run plotSpatial on each one
+
+dataFiles <- file.path(pp$data_dir,dir(pp$data_dir)[grep("\\.rda$",dir(pp$data_dir))])
+for(df in dataFiles){
+    pdfFile <- gsub("\\.rda","_spatial_plots.pdf",basename(df))
+    plotCellTypeLocations(df, pp$annotations_dir, pp$cell_types_file, pad=as.numeric(pp$pad), pdfFile=pdfFile,
+                verbose=pp$verbose, debug=pp$debug, logFile=pp$log)
+}
